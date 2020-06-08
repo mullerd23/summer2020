c7a94c9f81a777c68358407ffebaf11656f25837
Drew Muller <drewmuller@Drews-MacBook-Air-2.local>
2020-06-08 12:34:29 -0500

    Initial commit

---
+++ src/com/company/Myfirstprogram.java
@@ -0,0 +1,76 @@
+package com.company;
+
+import com.sun.tools.internal.xjc.reader.xmlschema.bindinfo.BIConversion;
+import sun.jvm.hotspot.HelloWorld;
+import sun.jvm.hotspot.HelloWorld;
+
+public class Myfirstprogram {
+
+    public static void main(String... args) {
+        System.out.println("HelloWorld");
+        System.out.println("  /\\");
+        System.out.println(" /  \\");
+        System.out.println("/    \\");
+        System.out.println("+-----+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+-----+");
+        System.out.println("|      |");
+        System.out.println("|      |");
+        System.out.println("+-----+");
+        System.out.println("  /\\");
+        System.out.println(" /  \\");
+        System.out.println("/    \\");
+
+
+        System.out.println("#================#");
+        System.out.println("|    <><>      |");
+        System.out.println("|  <>....<>    |");
+        System.out.println("| <>.......<>  |");
+        System.out.println("|<>..........<>|");
+        System.out.println("| <>........<> |");
+        System.out.println("|   <>.....<>  |");
+        System.out.println("|      <><>    |");
+        System.out.println("#================#");
+
+        System.out.println("+---------------+");
+        System.out.println("|       *       |");
+        System.out.println("|      /*\\     |");
+        System.out.println("|    // * \\\\  |");
+        System.out.println("|   /// * \\\\\\|");
+        System.out.println("|  \\\' * ///   |");
+        System.out.println("|    \\ *  //   |");
+        System.out.println("|    \' * /     |");
+        System.out.println("|       *       |");
+        System.out.println("+---------------+");
+        System.out.println("|\\\\\\ * ///   |");
+        System.out.println("|  \\\\ * //    |");
+        System.out.println("|    \\ * /     |");
+        System.out.println("|       *       |");
+        System.out.println("|       *       |");
+        System.out.println("|     / * \\    |");
+        System.out.println("|    // *  \\\\ |");
+        System.out.println("|   /// *  \\\\\\|");
+        System.out.println("+---------------+");
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/com/company/Rocket.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Rocket {
+
+    public static void main(String[] args) {
+        System.out.println("HelloWorld");
+        System.out.println("/\\");
+        System.out.println("/\\");
+        System.out.println("/\\");
+    }
+}
